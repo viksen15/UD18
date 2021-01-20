478d00cdca95659cde55f2f98190921ce24c4ea5
viksen <viksen_1234@hotmail.com>
2021-01-20 17:51:12 +0100

    Ej_1

---
+++ Ej_1/Ej_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Ej_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejecutable todo = new Ejecutable();
+            todo.ejecuta();
+        }
+    }
+}
