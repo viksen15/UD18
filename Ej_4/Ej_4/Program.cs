7b47bb6ab5015dbd2e6e872518fe3ad7dfad1b60
viksen <viksen_1234@hotmail.com>
2021-01-20 19:52:51 +0100

    Ej_4

---
+++ Ej_4/Ej_4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ej_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejecutable todo = new Ejecutable();
+            todo.ejecuta();
+        }
+    }
+}
